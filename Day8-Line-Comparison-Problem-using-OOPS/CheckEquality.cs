7a6fc3fcc747d0e9a0222d65424f7f5af66d1d49
rinkuberde <rinkuberde@gmail.com>
2023-01-11 00:18:08 +0530

    [Rinku] - Check equality of two lines

---
+++ Day8-Line-Comparison-Problem-using-OOPS/CheckEquality.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day8_Line_Comparison_Problem_using_OOPS
+{
+    internal class CheckEquality
+    {
+        public int x1, x2, y1, y2;
+
+        public CheckEquality(int x1, int x2, int y1, int y2)
+        {
+
+            this.x1 = x1;
+            this.x2 = x2;
+            this.y1 = y1;
+            this.y2 = y2;
+        }
+        public double FindingLength1()
+        {
+            //Calculating a line length
+
+            double LineLength = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            double len = Math.Round(LineLength, 2);
+            return len;
+
+        }
+            
+        public void EqualityMethod(string len1, string len2)
+        {   
+            //check Equality
+            if (len1 == len2)
+            {
+                Console.WriteLine("Lines are equal");
+            }
+            else
+            {
+                Console.WriteLine("Lines are not equal");
+            }
+        }
+    }
+}
--- Day8-Line-Comparison-Problem-using-OOPS/Program.cs
+++ Day8-Line-Comparison-Problem-using-OOPS/Program.cs
@@ -8,9 +8,20 @@
             // Console.WriteLine("Welcome to Line Comparison Problem using OOPS!!!");
 
             //Find length of line
-            FindLength obj1 = new FindLength(-6, 4, -4, 6);
-            double length = obj1.FindingLength();
-            Console.WriteLine("Length of Line is: " + length);
+             FindLength obj = new FindLength(-6, 4, -4, 6);
+             double length = obj.FindingLength();
+             Console.WriteLine("Length of Line is: " + length);
+
+            //Check equality of two lines
+            CheckEquality obj1 = new CheckEquality(4,3,2,-7);
+            double length1 = obj1.FindingLength1();
+            CheckEquality obj2 = new CheckEquality(5,1,9,7);
+            double length2 = obj2.FindingLength1();
+
+            Console.WriteLine("Length of First Line " + length1);
+            Console.WriteLine("Length of Second Line " + length2);
+            obj1.EqualityMethod(length1.ToString(), length2.ToString());
+
         }
     }
 }
