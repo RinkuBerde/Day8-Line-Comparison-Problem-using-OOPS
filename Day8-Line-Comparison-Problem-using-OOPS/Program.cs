8083179069cdcd6f198f347bd87b8d8c518cbc9d
rinkuberde <rinkuberde@gmail.com>
2023-01-10 22:57:03 +0530

    [Rinku] - Welcome Message

---
+++ Day8-Line-Comparison-Problem-using-OOPS/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day8_Line_Comparison_Problem_using_OOPS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Problem using OOPS!!!");
+        }
+    }
+}
